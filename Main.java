e04bc7e79adeaa27fdbd9e4c30312b16f687a2d3
LuisBrodely <luisbrodely2@gmail.com>
2022-12-16 02:43:56 -0600

    Programa con clase Math (metodo random)

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String [] args){
 
-        System.out.println("Hola Mundo :D");
+        System.out.println("Cambiando");
 
         Scanner in = new Scanner(System.in);
 
