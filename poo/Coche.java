e7dab634894d9e065e782a59fe4c946897900297
LuisBrodely <luisbrodely2@gmail.com>
2022-12-16 18:17:00 -0600

    Programación Orientada a Objetos

---
+++ poo/Coche.java
@@ -0,0 +1,18 @@
+package poo;
+
+public class Coche {
+    int ruedas;
+
+    String color;
+
+    int peso;
+    int ancho;
+
+    public Coche(){
+
+        ruedas = 4;
+        color = "gris";
+        peso = 500;
+        ancho = 2000;
+    }
+}
---
+++ poo/Main.java
@@ -0,0 +1,11 @@
+package poo;
+
+public class Main {
+    public static void main (String args[]){
+
+        Coche ferrari = new Coche(); //Instanciar una Clase / Ejemplar de Clase
+
+        System.out.println("Este coche tiene "+ ferrari.ruedas);
+
+    }
+}
