5cd8b7c98d6a6e1d85f33cb88ecb43a62c6d6e47
LuisBrodely <luisbrodely2@gmail.com>
2022-12-17 03:18:52 -0600

    Uso de static (variable global) para generar ID unico para cada empleado

--- prueba/Main.java
+++ prueba/Main.java
@@ -8,10 +8,15 @@ public class Main {
 
         Empleados empleado2 = new Empleados("Oscar");
 
+        Empleados empleado3 = new Empleados("Manuel");
+
         empleado2.setCambiarSeccion("Recursos Humanos");
 
         System.out.println(empleado1.getInformacionEmpleado());
+
         System.out.println(empleado2.getInformacionEmpleado());
+
+        System.out.println(empleado3.getInformacionEmpleado());
     }
 }
 
@@ -19,16 +24,20 @@ class Empleados{
 
     private final String nombre; //Al volver a la variable una constante una vez le das el nombre en el constructor
                                  // esta NO PUEDE CAMBIARSE (Palabra reservada "final")
-
     private String seccion;//Encapculación de variables
+
+    private int id;
+    private static int idSiguiente = 1;
     public Empleados(String nombre){
         this.nombre = nombre;
         seccion = "Administracion";
+        id = idSiguiente;
+        idSiguiente++;
     }
 
     //Getter
     public String getInformacionEmpleado(){
-        return "El nombre es: " + nombre + " y tu seccion es " + seccion;
+        return "El nombre es: " + nombre + " y tu seccion es " + seccion + " y tu ID: " + id;
     }
 
     //Setter
